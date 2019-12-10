36a7139ec7953fcf7613b232bb287e5aa817dea8
nurelayen <TPCF235018UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-12-10 13:45:57 -0500

    initialcommit

---
+++ src/main/java/com/example/demo/student.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class student {
+    private String name;
+    private int age;
+    private int height;
+    private long phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+}
